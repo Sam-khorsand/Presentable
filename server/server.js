bcddd4c4503bb6122a21eedb5b678a00548eada3
Sam Khorsand <khorsandsaeid@gmail.com>
2019-02-11 08:46:14 +0200

    heroku

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use("/records", records);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'));
+  app.use(express.static(path.resolve(__dirname, '../client', 'build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
