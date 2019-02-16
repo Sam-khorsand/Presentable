03af335c06c05c7caf39ed5573bbddc2a2775db6
Sam Khorsand <khorsandsaeid@gmail.com>
2019-02-16 16:21:55 +0200

    es5 style

--- client/src/helper.js
+++ client/src/helper.js
@@ -2,23 +2,23 @@ const axios = require('axios');
 
 module.exports = {
   dataAvailable: false,
-  queryRec: (selectedDate, cb) => {
+  queryRec: function (selectedDate, cb) {
     axios.get('/records', {
-        params: {
-          date: selectedDate
-        }
+      params: {
+        date: selectedDate
+      }
     })
-    .then(res => {
+      .then(res => {
         let records = ['date', 'sensor1', 'sensor2', 'sensor3', 'sensor4'], dailyRecs = []
         dailyRecs.push(records)
-        res.data.forEach(rec => {
-            let hourlyRecs = []
-            for (let key of records) 
-                hourlyRecs.push((key === 'date')  ? '' + new Date(rec[key]).getHours() : rec[key])
-            dailyRecs.push(hourlyRecs)
+        res.data.map(rec => {
+          let hourlyRecs = []
+          for (let key of records)
+            hourlyRecs.push((key === 'date') ? '' + new Date(rec[key]).getHours() : rec[key])
+          dailyRecs.push(hourlyRecs)
         });
-        (dailyRecs.length > 1 ) ? this.dataAvailable = true : this.dataAvailable = false;
+        (dailyRecs.length > 1) ? this.dataAvailable = true : this.dataAvailable = false;
         cb(dailyRecs);
-    })
+      })
   },
 }
